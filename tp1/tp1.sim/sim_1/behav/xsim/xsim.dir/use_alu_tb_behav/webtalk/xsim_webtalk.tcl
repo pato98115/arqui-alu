6d4a29e6675adb5061345ff905b7ba86dcaac7c3
pato98115 <pato98115@gmail.com>
2021-10-20 21:29:08 -0300

    use_alu_tb simulation not working

--- tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/xsim_webtalk.tcl
+++ tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Oct 20 21:16:39 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Oct 20 21:24:57 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2021.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3247384" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "46585f98-5f15-420e-a0c4-5eca493b2405" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "292bfd3cf49b44f5b170b7b8bea0fe79" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 20.04.2 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-9300H CPU @ 2.40GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.10_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "116728_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1089412421 -regid "" -xml /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 3020366305 -regid "" -xml /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/patricio/Facultad/arqui/practico/arqui-alu/tp1/tp1.sim/sim_1/behav/xsim/xsim.dir/use_alu_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
